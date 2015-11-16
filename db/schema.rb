2b97debec4afa1f8f1f6b4423d57bbe7c58c71be
Max Lovric <mmax@hotmail.co.uk>
2015-11-16 20:57:37 +0000

    Modified devise for stripe

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150812204738) do
+ActiveRecord::Schema.define(version: 20151116203556) do
 
   create_table "contacts", force: :cascade do |t|
     t.string   "name"
