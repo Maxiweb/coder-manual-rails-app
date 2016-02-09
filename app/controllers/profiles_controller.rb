00ab9df96689571815e22df9516cc1da9ae84157
Max Lovric <mmax@hotmail.co.uk>
2016-02-09 20:44:19 +0000

    Added before filters to check correct user

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,28 +1,45 @@
 class ProfilesController < ApplicationController
-    def new
+  before_action :authenticate_user!
+  before_action :only_current_user
+  def new
     # form where a user can fill out their own profile.
-    @user = User.find( [:user_id] )
+    @user = User.find( params[:user_id] )
     @profile = Profile.new
-    end
-    
-    def create
-    @user = User.find( [:user_id] )
+  end
+  
+  def create 
+    @user = User.find( params[:user_id] )
     @profile = @user.build_profile(profile_params)
-        if @profile.save
-          flash[:success] = "Profile Updated!"
-          redirect_to user_path( params[:user_id] )
-        else
-          render action: :new
-        end
+    if @profile.save
+      flash[:success] = "Profile Updated!"
+      redirect_to user_path( params[:user_id] )
+    else
+      render action: :new
     end
-    
-    def edit
-        @user = User.find( params[:user_id] )
-        @profile = @user.profile
+  end
+  
+  def edit
+    @user = User.find( params[:user_id] )
+    @profile = @user.profile
+  end
+  
+  def update
+    @user = User.find( params[:user_id] )
+    @profile = @user.profile
+    if @profile.update_attributes(profile_params)
+      flash[:success] = "Profile Updated!"
+      redirect_to user_path( params[:user_id] )
+    else
+      render action: :edit
     end
-    
-    private
-        def profile_params
-            params.require(:profile).permit(:first_name, :last_name, :job_title, :phone_number, :contact_email, :description)
-        end
+  end
+  
+  private
+    def profile_params
+      params.require(:profile).permit(:first_name, :last_name, :job_title, :phone_number, :contact_email, :description)
+    end
+    def only_current_user
+      @user = User.find( params[:user_id] )
+      redirect_to(root_url) unless @user == current_user
+    end
 end
